afebc71e929223003d6f02c5319df2e337eb0dbb
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-11-19 12:38:13 +0530

    exe

---
+++ one.py
@@ -0,0 +1,28 @@
+line1='****************'
+line2='                '
+line3='Welcome back abi'
+line4='                '
+line5='****************'
+
+print(line1)
+print(line2)
+print(line3)
+print(line4)
+print(line5)
+
+
+#-------0r--------------------
+
+line1='****************'
+line2='                '
+line3='Welcome back abi'
+
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)    
+
+
+
+
---
+++ shutdown.py
@@ -0,0 +1,9 @@
+import os
+os.system("shutdown /s /t 1")
+
+
+#----------restart-----------
+
+
+import os
+os.system("shutdown /r /t 1")
